a9b1487e2b6cf56845dbf3a77ad34362b6c224a4
nicro <dev.nicro@gmail.com>
2022-04-13 12:58:00 +0100

    replace(main.ts): require -> import

--- src/main.ts
+++ src/main.ts
@@ -17,11 +17,13 @@ function loadEvents() {
         .filter(file => file.endsWith('.ts'));
 
     for (const file of eventFiles) {
-        const event = require(`./events/${file.replace('.ts', '.js')}`);
-        if (event.once)
-            client.once(event.name, (...args) => event.execute(...args));
-        else
-            client.on(event.name, (...args) => event.execute(...args));
+        import(`./events/${file.replace('.ts', '.js')}`)
+        .then(event => {
+            if (event.once)
+                client.once(event.name, (...args) => event.execute(...args));
+            else
+                client.on(event.name, (...args) => event.execute(...args));
+        });
     }
 }
 
